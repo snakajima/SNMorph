798de25e84e7c4cc4f8ef3c8835f5583d5708120
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2016-10-02 06:40:20 +0900

    pinch & zoom

--- SNMorph/SNMorph/src/SNMorphEditController.swift
+++ SNMorph/SNMorph/src/SNMorphEditController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
+// from SNDraw
+private extension CGPoint {
+    func delta(from:CGPoint) -> CGPoint {
+        return CGPoint(x: self.x - from.x, y: self.y - from.y)
+    }
+}
+
 class SNMorphEditController: UIViewController {
     public var grid:SNMorphGrid!
     
     @IBOutlet var viewMain:UIView!
+
+    private var xform = CGAffineTransform.identity
+    // Transient properties for handlePinch
+    private var anchor = CGPoint.zero
+    private var delta = CGPoint.zero
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +37,28 @@ class SNMorphEditController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func handlePinch(recognizer:UIPinchGestureRecognizer) {
+        let ptMain = recognizer.location(in: viewMain)
+        let ptView = recognizer.location(in: view)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        switch(recognizer.state) {
+        case .began:
+            anchor = ptView
+            delta = ptMain.delta(from: viewMain.center)
+        case .changed:
+            if recognizer.numberOfTouches == 2 {
+                var offset = ptView.delta(from: anchor)
+                offset.x /= xform.a
+                offset.y /= xform.a
+                var xf = xform.translatedBy(x: offset.x + delta.x, y: offset.y + delta.y)
+                xf = xf.scaledBy(x: recognizer.scale, y: recognizer.scale)
+                xf = xf.translatedBy(x: -delta.x, y: -delta.y)
+                self.viewMain.transform = xf
+            }
+        case .ended:
+            xform = self.viewMain.transform
+        default:
+            self.viewMain.transform = xform
+        }
     }
-    */
-
 }
