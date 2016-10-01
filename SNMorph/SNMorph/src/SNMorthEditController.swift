27907adeb67ce895274fd262293eb8d37cd3b437
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2016-10-01 20:48:01 +0900

    SNMorthEdit

---
+++ SNMorph/SNMorph/src/SNMorthEditController.swift
@@ -0,0 +1,35 @@
+//
+//  SNMorthEditController.swift
+//  SNMorph
+//
+//  Created by satoshi on 10/1/16.
+//  Copyright © 2016 Satoshi Nakajima. All rights reserved.
+//
+
+import UIKit
+
+class SNMorthEditController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("viewDidLoad")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
